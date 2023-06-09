f67a01fff09e7ce1ba515b32eb119908cf1faadb
Manoel-Micherlane <98850488+Manoel-Micherlane@users.noreply.github.com>
2023-06-09 16:38:11 -0300

    Create q5.c

---
+++ q5.c
@@ -0,0 +1,15 @@
+/* 5. Escreva um programa que mostra todos os números pares de 1 até 100.
+Esse não precisa nem de código, basta você alterar apenas um número
+do exercício anterior.
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    for(int c = 0; c <=100; c +=2)
+    {
+        printf("%i\n", c);
+    }
+}
