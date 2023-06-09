bee6b16888c811453e429ab43cfba489791f8d0b
Manoel-Micherlane <98850488+Manoel-Micherlane@users.noreply.github.com>
2023-06-09 16:37:18 -0300

    Create q4.c

---
+++ q4.c
@@ -0,0 +1,13 @@
+/* 4. Escreva um programa que mostra todos os números ímpares de 1 até
+100. */
+
+
+#include <stdio.h>
+
+int main()
+{
+    for(int c = 1; c < 100; c +=2)
+    {
+        printf("%i\n", c);
+    }
+}
